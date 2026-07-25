c1453eb19a2e55d41524c832a6509ea2dde1e5fd
Kym Carabeo <yuudachipoikancolle@gmail.com>
2026-07-25 19:57:14 +0800

    completed exercise 3.12

---
+++ phonebook/backend/mongo.js
@@ -0,0 +1,53 @@
+const mongoose = require('mongoose')
+
+if (process.argv.length < 3) {
+  console.log('give password as argument')
+  process.exit(1)
+}
+
+const password = process.argv[2]
+
+const url = `mongodb+srv://denrncrkvcarabeo:${password}@cluster0.rswxxkc.mongodb.net/phonebookApp?retryWrites=true&w=majority&appName=Cluster0`
+
+mongoose.set('strictQuery', false)
+mongoose.connect(url, { family: 4 })
+
+const personSchema = new mongoose.Schema({
+  name: String,
+  number: String,
+})
+
+const Person = mongoose.model('Person', personSchema)
+
+// MODE 1: Show all entries if ONLY password is given
+if (process.argv.length === 3) {
+  Person.find({}).then(result => {
+    console.log('phonebook:')
+    result.forEach(person => {
+      console.log(`${person.name} ${person.number}`)
+    })
+    mongoose.connection.close()
+  })
+}
+
+// MODE 2: Add new entry if password, name, AND number are given
+else if (process.argv.length >= 5) {
+  const name = process.argv[3]
+  const number = process.argv[4]
+
+  const person = new Person({
+    name: name,
+    number: number,
+  })
+
+  person.save().then(() => {
+    console.log(`added ${name} number ${number} to phonebook`)
+    mongoose.connection.close()
+  })
+}
+
+// INVALID USAGE: Missing number or too many args
+else {
+  console.log('Please provide both name and number to add an entry, e.g.: node mongo.js <password> <name> <number>')
+  mongoose.connection.close()
+}
