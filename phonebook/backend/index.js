94f6c5a12d9778f23702ff4f39af6d750ffcabb2
Kym Carabeo <yuudachipoikancolle@gmail.com>
2026-07-27 09:13:51 +0800

    completed exercise 3.15-3.18

--- phonebook/backend/index.js
+++ phonebook/backend/index.js
@@ -116,5 +116,5 @@ app.use(errorHandler)
 
 const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`)
+  console.log(`Server running on port 127.0.0.1:${PORT}`)
 })
